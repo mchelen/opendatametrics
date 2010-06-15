972f89d8df5931dc4ddb9c16094cd0041eba8aee
Mike Chelen <michael.chelen@gmail.com>
2010-06-15 13:51:21 +0000

    updated pmc ftp xml script

--- pmc/pmc.ftp.xml.sh
+++ pmc/pmc.ftp.xml.sh
@@ -1,36 +1,34 @@
 #!/bin/sh
 # requires curlftpfs
-
+# author: Michael Chelen http://mikechelen.com http://twitter.com/mikechelen
+# license: Creative Commons Zero
 # file list: http://www.ncbi.nlm.nih.gov/pmc/about/ftp.html#XML_for_Data_Mining
-
-# mount path
-mountpath=mount
 # output path
 outputpath=output
-# generate output file name
-outputfile=${ftp}.$(echo $ftppath|sed 's/\//./g').$(date +%s).tsv
-
 # ftp server and remote path
 ftp=ftp.ncbi.nlm.nih.gov
 ftppath=pub/pmc
-
+# generate output file name
+outputfile=${ftp}.$(echo $ftppath|sed 's/\//./g').$(date +%s).tsv
+outputerror=${ftp}.$(echo $ftppath|sed 's/\//./g').$(date +%s).error.txt
 # create mount point
-mkdir -p $mountpath
-
+mkdir -p $ftp
 # mount ftp with curlftpfs
-curlftpfs $ftp $mountpath
-
+echo Mounting $ftp
+curlftpfs $ftp $ftp
 # create output path
 mkdir -p $outputpath
-
 # output file list
-tar -tvf $mountpath/$ftppath/articles.A-B.tar.gz >> $outputpath/$outputfile 2>> errors.txt
-tar -tvf $mountpath/$ftppath/articles.C-H.tar.gz >> $outputpath/$outputfile 2>> errors.txt
-tar -tvf $mountpath/$ftppath/articles.I-N.tar.gz >> $outputpath/$outputfile 2>> errors.txt
-tar -tvf $mountpath/$ftppath/articles.O-Z.tar.gz >> $outputpath/$outputfile 2>> errors.txt
-
-# unmount
-fusermount -uz $mountpath
-
-# remove mount path
-rmdir $mountpath
+outputpathfile=$outputpath/$outputfile
+outputerrorpathfile=$outputpath/$outputerror
+for I in A-B C-H I-N O-Z;
+do
+ echo $I;
+ curpath=$ftp/$ftppath/articles.$I.tar.gz
+ echo Opening $curpath for output to $outputpathfile
+ tar -tvf $curpath >> $outputpathfile 2>> $outputerrorpathfile
+ echo Done with $curpath
+done
+# unmount and remove directory 
+fusermount -uz $ftp
+rmdir $ftp
