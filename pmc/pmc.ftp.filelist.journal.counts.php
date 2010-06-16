8c6ff87fbc6e2afcfdbda088cb3e825c0eeb827d
Mike Chelen <mike@michaelchelen.net>
2010-06-16 11:34:18 -0400

    new php script for journal counts in pmc ftp list

---
+++ pmc/pmc.ftp.filelist.journal.counts.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * author: Michael Chelen http://mikechelen.com http://twitter.com/mikechelen
+ * license: Creative Commons Zero
+ * downloads pmc open access subset ftp file list and computes article counts for each journal
+ * source: ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/file_list.txt
+ */
+
+// current unix timestamp
+$now = time();
+
+// file url
+$url = "ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/file_list.txt";
+
+$filename = "file_list.".$now.".txt";
+
+// download file with wget
+// $output = `wget -O $filename $url`;
+
+// download file with aria2c
+$output = `aria2c -o $filename $url`;
+
+
+// open file
+$handle = fopen($filename, "r");
+
+// initialize journal array
+$journals = array();
+
+// read file as csv (tsv)
+while (($data = fgetcsv($handle, 0, chr(9))) !== FALSE) {
+// loop through the fields in the row
+  preg_match('/^[^0-9]+/', $data[1],$matches);
+  echo $matches[0];
+}
+// close file
+fclose($handle);
+
+
+
+
+?>
